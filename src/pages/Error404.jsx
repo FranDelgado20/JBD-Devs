9aab95e14a78fc3fbda5582ab84a74533b1d6cc1
Tomas <tomibudeguer20@gmail.com>
2024-02-19 17:54:40 -0300

    agregue imagen de erro 404

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Error404 = () => {
+  return (
+    <Container fluid className="">
+      <div className="bg-[url('http://imgfz.com/i/F3PqbDZ.png')] bg-cover bg-center ">
+        <div>
+          <img
+            src="/Diseño_sin_título-removebg-preview.png "
+            alt="error"
+            className=" mx-auto  "
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Error404;
