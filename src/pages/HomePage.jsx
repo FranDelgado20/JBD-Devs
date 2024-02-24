037704ad89dd8c61021856d2f27e251f6a158837
Tomas <tomibudeguer20@gmail.com>
2024-02-23 23:29:57 -0300

    agregue una funcion para cambiar de parafo, cambie fuente y agregue animaciones

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,33 +27,41 @@ const members = [
 
 const HomePage = () => {
   const [open, setOpen] = useState(1);
+  const [primerParr, setPrimerParr] = useState(true);
+
+  const cambiarParrUno = () => {
+    setPrimerParr(true);
+  };
+  const cambiarParrDos = () => {
+    setPrimerParr(false);
+  };
 
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
     <>
       <Container fluid>
-        <section className="min-h-screen py-8 px-8 lg:py-28">
-          <div className="d-flex justify-center">
+        <section className="min-h-screen py-8 px-8">
+          <div className="d-flex justify-center" data-aos="zoom-in">
             <Breadcrumbs
               className="rounded-full bg-gradient-to-tr color-card justify-center p-2"
               separator={<span className="text-white ">/</span>}
             >
               <a
                 href="#cardsMembers"
-                className="rounded-full bg-white px-3 py-1 font-medium text-gray-900 no-underline bread"
+                className="rounded-full bg-neutral-100 px-3 py-1 font-medium text-gray-900 no-underline bread transition hover:bg-neutral-700 hover:text-neutral-100"
               >
                 Nosotros
               </a>
               <a
                 href="#union"
-                className="rounded-full bg-white px-3 py-1 font-medium text-gray-900 no-underline bread"
+                className="rounded-full bg-neutral-100 px-3 py-1 font-medium text-gray-900 no-underline bread transition hover:bg-neutral-700 hover:text-neutral-100"
               >
                 Juntos
               </a>
               <a
                 href="#proyectos"
-                className="rounded-full bg-white px-3 py-1 font-medium text-gray-900 no-underline bread"
+                className="rounded-full bg-neutral-100 px-3 py-1 font-medium text-gray-900 no-underline bread transition hover:bg-neutral-700 hover:text-neutral-100"
               >
                 Proyectos
               </a>
@@ -61,45 +69,112 @@ const HomePage = () => {
           </div>
 
           <div className="container mx-auto">
-            <div className="mb-16 text-center lg:mb-28">
-              <Typography variant="h2" className="!text-gray-500 ">
+            <div className="mb-8 text-center lg:mb-28" data-aos="zoom-in-down">
+              <h2 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-gray-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
                 ¡Conozca a nuestro equipo!
-              </Typography>
-              <Typography
+              </h2>
+              {/* <Typography variant="h2" className="!text-gray-500 ">
+                ¡Conozca a nuestro equipo!
+              </Typography> */}
+              {/* <Typography
                 variant="h1"
                 className="my-2 !text-2xl lg:!text-4xl !text-gray-500"
               >
                 JBD Devs
-              </Typography>
-              <Typography
-                variant="lead"
-                className="mx-auto w-full !text-gray-400 max-w-4xl text-justify tamaño-cel"
-              >
-                Somos un equipo apasionado de desarrolladores web dedicados a
-                hacer realidad tus ideas digitales. Con una combinación única de
-                creatividad, experiencia y compromiso, estamos aquí para llevar
-                tus proyectos al siguiente nivel. En JBD Devs, no solo creamos
-                sitios web, creamos experiencias digitales cautivadoras. Desde
-                el diseño hasta la implementación, nos esforzamos por brindar
-                soluciones innovadoras y funcionales que destaquen en el vasto
-                mundo en línea. Nuestra misión es simple: transformar tus
-                conceptos en realidad digital. Ya sea que necesites una elegante
-                página de presentación, una tienda en línea robusta o una
-                aplicación web personalizada, estamos listos para trabajar
-                contigo para hacer que tu visión cobre vida. Con un enfoque
-                centrado en el cliente y una pasión por la excelencia técnica,
-                nos comprometemos a proporcionar resultados excepcionales en
-                cada proyecto que emprendemos. Tu satisfacción y éxito son
-                nuestra prioridad número uno. ¡Únete a nosotros en el viaje
-                hacia la excelencia digital! Estamos emocionados de colaborar
-                contigo y llevar tus ideas al mundo en línea.
-              </Typography>
-              <Typography
+              </Typography> */}
+              <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-gray-600 md:text-5xl lg:text-5xl lg:max-w-7xl">
+                JBD Devs
+              </h1>
+              {primerParr ? (
+                <Typography
+                  variant="lead"
+                  className="mx-auto w-full font-bold !text-gray-400 max-w-4xl text-justify tamaño-cel"
+                >
+                  Somos un equipo apasionado de desarrolladores web dedicados a
+                  hacer realidad tus ideas digitales. Con una combinación única
+                  de creatividad, experiencia y compromiso, estamos aquí para
+                  llevar tus proyectos al siguiente nivel. En JBD Devs, no solo
+                  creamos sitios web, creamos experiencias digitales
+                  cautivadoras. Desde el diseño hasta la implementación, nos
+                  esforzamos por brindar soluciones innovadoras y funcionales
+                  que destaquen en el vasto mundo en línea. Nuestra misión es
+                  simple: transformar tus conceptos en realidad digital.
+                </Typography>
+              ) : (
+                <Typography
+                  variant="lead"
+                  className="mx-auto w-full font-bold !text-gray-400 max-w-4xl text-justify tamaño-cel"
+                >
+                  Ya sea que necesites una elegante página de presentación, una
+                  tienda en línea robusta o una aplicación web personalizada,
+                  estamos listos para trabajar contigo para hacer que tu visión
+                  cobre vida. Con un enfoque centrado en el cliente y una pasión
+                  por la excelencia técnica, nos comprometemos a proporcionar
+                  resultados excepcionales en cada proyecto que emprendemos. Tu
+                  satisfacción y éxito son nuestra prioridad número uno. ¡Únete
+                  a nosotros en el viaje hacia la excelencia digital! Estamos
+                  emocionados de colaborar contigo y llevar tus ideas al mundo
+                  en línea.
+                </Typography>
+              )}
+              <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-gray-400 md:text-5xl lg:text-2xl lg:max-w-7xl mb-3">
+                ¡JBD Devs - Donde la creatividad encuentra la tecnología!
+              </h1>
+              {/* <Typography
                 variant="lead"
                 className="mx-auto w-full !text-gray-400 max-w-4xl"
               >
                 ¡JBD Devs - Donde la creatividad encuentra la tecnología!
-              </Typography>
+              </Typography> */}
+              {primerParr ? (
+                <nav aria-label="Breadcrumb" className="flex justify-center">
+                  <ol className="flex overflow-hidden rounded-lg border border-gray-200 text-gray-600">
+                    <li className="flex items-center">
+                      <button
+                        onClick={cambiarParrUno}
+                        className=" flex h-10 items-center gap-1.5 bg-gray-100 px-4 transition text-neutral-900  -ms-8 hover:text-neutral-900 "
+                      >
+                        <span className=" text-s font-bold"> JBD </span>
+                      </button>
+                    </li>
+
+                    <li className="relative flex items-center">
+                      <span className="absolute inset-y-0 -start-px h-10 w-4 bg-gray-100 [clip-path:_polygon(0_0,_0%_100%,_100%_50%)] rtl:rotate-180"></span>
+
+                      <button
+                        onClick={cambiarParrDos}
+                        className="flex text- h-10 items-center bg-neutral-950 pe-4 ps-8 text-s font-bold transition text-neutral-100 hover:text-neutral-400"
+                      >
+                        Devs
+                      </button>
+                    </li>
+                  </ol>
+                </nav>
+              ) : (
+                <nav aria-label="Breadcrumb" className="flex justify-center">
+                  <ol className="flex overflow-hidden rounded-lg border border-gray-200 text-gray-600">
+                    <li className="flex items-center ">
+                      <button
+                        onClick={cambiarParrUno}
+                        className=" flex h-10 items-center gap-1.5 px-4 transition -ms-8 bg-neutral-950 text-neutral-100  hover:text-neutral-400 "
+                      >
+                        <span className=" text-s font-bold"> JBD </span>
+                      </button>
+                    </li>
+
+                    <li className="relative flex items-center">
+                      <span className="absolute inset-y-0 -start-px h-10 w-4 bg-neutral-950  [clip-path:_polygon(0_0,_0%_100%,_100%_50%)] rtl:rotate-180 transition"></span>
+
+                      <button
+                        onClick={cambiarParrDos}
+                        className="flex  h-10 items-center bg-gray-100 pe-4 ps-8 text-s font-bold transition   hover:text-neutral-100 text-black"
+                      >
+                        Devs
+                      </button>
+                    </li>
+                  </ol>
+                </nav>
+              )}
             </div>
 
             <Row id="cardsMembers">
